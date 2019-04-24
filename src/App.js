cd8e41a6a516aee2e28a7f6df31bd388d5b1c745
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-04-24 07:22:30 +0200

    All operation buttons now available - not working. Methods attached to operation buttons. Methods not yet defined. Operation state key is now available.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "./css/App.css";
 import Display from "./Display";
 // import Number from "./Number";
@@ -9,17 +9,19 @@ class App extends Component {
     this.state = {
       memory: "",
       display: "",
-      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+      operation: "",
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."]
     }
-    
+
     this.handleClick = this.handleClick.bind(this);
     this.handleClear = this.handleClear.bind(this);
+    this.handleClearAll = this.handleClearAll.bind(this);
   }
 
   handleClick(event) {
-    const {name} = event.target;
+    const { name } = event.target;
     this.setState(prevState => {
-      return {display: prevState.display + name,}
+      return { display: prevState.display + name, }
     })
   }
 
@@ -29,30 +31,83 @@ class App extends Component {
     })
   }
 
+  handleClearAll() {
+    this.setState({
+      memory: "",
+      display: "",
+    })
+  }
+
   render() {
     let nums = this.state.numbers
     return (
       <div className="calculator">
-        <Display data={this.state.display}/>
-        <button
-          className="clear"
-          onClick={this.handleClear}
-        >
-          Clear
-        </button>
+        <Display data={this.state.display} />
         <div className="buttons-container">
-          {
-            nums.map(num => 
-            <button 
-              name={num.toString()}
-              key={num}
-              className="number"
-              id={num.toString()}
-              onClick={this.handleClick} 
-            >
-              {num.toString()}
-            </button>)
-          }
+          <div className="column">
+            <div className="clear-container">
+              <button
+                className="clear"
+                onClick={this.handleClear}
+              >
+                Clear
+              </button>
+              <button
+                className="clear"
+                onClick={this.handleClearAll}
+              >
+                Clear All
+              </button>
+            </div>
+            <div className="numpad-container">
+              {
+                nums.map(num =>
+                  <button
+                    name={num.toString()}
+                    key={num}
+                    className="number"
+                    id={num.toString()}
+                    onClick={this.handleClick}
+                  >
+                    {num.toString()}
+                  </button>)
+              }
+            </div>
+          </div>
+          <div className="column">
+              <div className="operations-container">
+                <button
+                  name="backspace"
+                  onClick={this.handleBackspace}
+                >
+                {"<--"}
+                </button>
+                <button
+                  name="plus"
+                  onClick={this.handlePlus}
+                >
+                +
+                </button>
+                <button
+                  name="minus"
+                  onClick={this.handleMinus}
+                >
+                -
+                </button>
+                <button
+                  name="divide"
+                  onClick={this.handleDivide}
+                >
+                /
+                </button>
+                <button
+                  name="multiply"
+                  onClick={this.handleMultiply}
+                >
+                *
+                </button>
+              </div>
+          </div>
         </div>
       </div>
     )

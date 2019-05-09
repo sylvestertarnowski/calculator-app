03814eeb007589350e97bf4f740079bdb4ec1c26
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-05-09 22:03:25 +0200

    More tests written

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,13 +1,26 @@
+describe('Second test', () => {
+    it('Visit the app', () => {
+        cy.visit('/');
+
+        cy.contains('CA').click();
+        cy.contains('1').click();
+        cy.contains('2')
+    });
+});
+
 describe('My First Test', function() {
     it('Does not do much!', function() {
         expect(true).to.equal(true);
     })
 })
 
-describe('Second test', () => {
-    it('Visit the app', () => {
-        cy.visit('/');
+describe('Operators behaviour', () => {
+    it('Click multiple operators in a row', () => {
+        cy.contains('5').click();
+        cy.contains('/').click();
+        cy.contains('1').click();
+        cy.contains('/').click();
+        cy.contains('+').click();
 
-        cy.contains('CA');
-    });
-});
+    })
+})
